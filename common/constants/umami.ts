51dc5d7de6f689fbb71794a86c64e14ebe64d200
syarif32 <najwasyarif563@g,ail.com>
2026-02-27 23:39:59 +0700

    umami views

--- common/constants/umami.ts
+++ common/constants/umami.ts
@@ -4,20 +4,20 @@ export const UMAMI_ACCOUNT = {
   base_url: "https://api.umami.is/v1/websites",
   endpoint: {
     page_views: "/pageviews",
-    sessions: "/stats", 
+    sessions: "/sessions/stats",
   },
   parameters: {
-    startAt: Date.now() - (30 * 24 * 60 * 60 * 1000), 
-    endAt: Date.now(), 
-    unit: "day",
+    startAt: 1717174800000, // 1 Juni 2024 00:00 WIB
+    endAt: 1767190799000, // 31 Desember 2025 23:59 WIB
+    unit: "month",
     timezone: "Asia/Jakarta",
   },
   is_active: true,
   websites: [
     {
-      domain: "zaeeon-site.vercel.app", 
-      website_id: process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID, 
-      umami_url: "https://cloud.umami.is/share/ID_SHARE_BOS", 
+      domain: "zaeeon-site.vercel.app",
+      website_id: process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID,
+      umami_url: "https://cloud.umami.is/share/ID_SHARE_BOS", // Kosongin aja kalau belum ada
     },
   ],
 };
